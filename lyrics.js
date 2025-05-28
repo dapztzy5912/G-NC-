adff7b71d2426a3d9a1bf96741adb7f01c6f677c
dapztzy5912 <depannation999@gmail.com>
2025-05-29 00:12:16 +0700

    Create lyrics.js

---
+++ lyrics.js
@@ -0,0 +1,10 @@
+export const lyrics = [
+  "Karna kamu cantik",
+  "Kan kuberi segalanya apa yang kupunya",
+  "Dan hatimu baik",
+  "Sempurnalah duniaku saat kau di sisiku",
+  "Bukan karna make up di wajahmu",
+  "Atau lipstik merah itu",
+  "Lembut hati tutur kata",
+  "Terciptalah cinta yang kupuja"
+];
